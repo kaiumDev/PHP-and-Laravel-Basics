a05a9af8db12eeba0e376707f36f94bb356fefd4
Md AL Kaium <kaium.github.cse@gmail.com>
2024-07-11 12:17:29 +0600

    new project

--- example-app/resources/views/ContactPage.Blade.php
+++
@@ -1,1 +0,0 @@
-<h1>Contactpage</h1>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',function(){
-    return view('HomePage');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/contact',function(){
-    return view('ContactPage');
-});
-
-Route::get('/about',function(){
-    return view('AboutPage');
+Route::get('/', function () {
+    return view('welcome');
 });
